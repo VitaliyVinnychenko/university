66510a1382def3c4f73e79a98e74bdf750cd2423
Vitaliy Vinnychenko <vitaliy.vinnychenko.home@gmail.com>
2017-10-15 18:26:12 +0300

    [*] code style fixed - numerical methods

--- Second course - First period/Numerical Methods/5th lab/5th lab/main.cpp
+++ Second course - First period/Numerical Methods/5th lab/5th lab/main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
+
 const double A = 1.0, B = 2.0, N = 1000000;
+
 
 double f(double x) {
     return x * pow(2, 2 * x);
 }
-
 
 int main(int argc, const char * argv[]) {
     double integral = 0, x = A, h = (B - A) / N;
@@ -19,7 +20,7 @@ int main(int argc, const char * argv[]) {
     integral *= h;
     
     cout << 2 * integral << endl;
-    
     cout << f(B) - f(A) << endl << endl;
+    
     return 0;
 }

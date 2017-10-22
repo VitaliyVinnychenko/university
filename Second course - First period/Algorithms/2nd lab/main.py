aaf788e111227f1e8d66659813c8f198669b4e75
Vitaliy Vinnychenko <vitaliy.vinnychenko.home@gmail.com>
2017-10-23 00:01:53 +0300

    [+] add 2nd lab - algorithms

--- Second course - First period/Algorithms/2nd lab/main.py
+++ Second course - First period/Algorithms/2nd lab/main.py
@@ -1,101 +1,43 @@
-import sys
+array = sorted(map(int, open('lngpok.in', 'r').read().split()))
+zeroes_number = 0
+
+for i in array:
+    if i != 0:
+        break
+
+    zeroes_number += 1
 
 
-def get_input_array():
-    args = sys.argv[1:]
+array = sorted(list(set(array)))
 
-    if len(args) == 2 and args[0] == '-input':
-        return [int(i) for i in open(args[1], 'r').read().split(' ')]
+if zeroes_number != 0:
+    array = array[1:]
 
-    return None
+array_size = len(array)
+result = 0
 
 
-def quicksort(input_array, left, right):
+for i in range(array_size):
+    tmp = zeroes_number
+    count = 1
 
-    i = left
-    j = right
-    pivot = input_array[round((left + right) / 2)]
+    for j in range(i + 1, array_size):
+        gap = array[j] - array[j - 1]
 
-    while left < j or i < right:
-        while input_array[i] < pivot:
-            i = i + 1
-
-        while input_array[j] > pivot:
-            j = j - 1
-
-        if i <= j:
-            input_array[i], input_array[j] = input_array[j], input_array[i]
-            i = i + 1
-            j = j - 1
-
+        if gap == 1:
+            count += 1
+        elif gap == 2:
+            count += 2
+            tmp -= 1
+        elif tmp >= gap - 1 > 2 and tmp > 0:
+            tmp -= gap - 1
+            count += gap
         else:
-            if left < j:
-                quicksort(input_array, left, j)
+            break
 
-            if i < right:
-                quicksort(input_array, i, right)
+    count += tmp
 
-            return
+    if result < count:
+        result = count
 
-
-def filter_array(array):
-    zero_number = 0
-    unique_cards = []
-    flag = True
-
-    for i in range(0, len(array) - 1):
-
-        if array[i] == 0:
-            zero_number = zero_number + 1
-        else:
-            if flag:
-                unique_cards.append(array[i])
-                flag = False
-
-            if not array[i] == array[i + 1]:
-                unique_cards.append(array[i + 1])
-
-    return unique_cards, zero_number
-
-
-def get_max_collection(array):
-
-    unique_cards, zero_number = filter_array(array)
-    unique_cards_number = range(1, len(unique_cards))
-    max_length = 0
-
-    for i in unique_cards:
-        counter = zero_number + 1
-        quota = zero_number
-
-        for j in unique_cards_number:
-            difference = unique_cards[j] - unique_cards[j - 1]
-
-            if difference == 1:
-                counter = counter + 1
-            elif difference == 2 and quota > 0:
-                counter += 1
-                quota -= 1
-            else:
-                break
-
-        if counter > max_length:
-            max_length = counter
-
-    return max_length
-
-
-def main():
-    array = get_input_array()
-
-    if array is None:
-        print('Enter: python3 main.py -input <input file>')
-    else:
-        quicksort(array, 0, len(array) - 1)
-        result = get_max_collection(array)
-
-        print(result)
-
-
-if __name__ == '__main__':
-    main()
+print(result)

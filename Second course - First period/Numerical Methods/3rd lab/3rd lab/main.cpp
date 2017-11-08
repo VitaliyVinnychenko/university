005df8cae0be89dda020f01e8cd4537517fd834d
Vitaliy Vinnychenko <vitaliy.vinnychenko.home@gmail.com>
2017-11-08 10:31:02 +0200

    [+] add 3rd lab - numerical methods

--- Second course - First period/Numerical Methods/3rd lab/3rd lab/main.cpp
+++ Second course - First period/Numerical Methods/3rd lab/3rd lab/main.cpp
@@ -1,44 +1,152 @@
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <math.h>
-#include <vector>
-using namespace std;
+#include <time.h>
 
-short k = 0;
-const short n = 2;
-const double eps = 0.00001, X = 1.5, Y = -1.5, H = 0.01;
-vector<double> x, y, old_x, old_y;
+#define SQR(X) ((X)*(X))
+#define N 2
+#define EPS 0.00001
 
+double f0(double x[N]) { return 4 * SQR(x[0]) + SQR(x[1]) - 4; }
+double f1(double x[N]) { return x[0] - SQR(x[1]) + 1; }
 
-double function1(double x, double y) {
-    return 4 * x * x + y * y - 4;
+double f00(double x[N]) { return 8 * x[0] + 2 * x[1]; }
+double f01(double x[N]) { return 1 - 2 * x[1]; }
+
+double f10(double x[N]) { return 10; }
+double f11(double x[N]) { return -2; }
+
+double x[N] = {
+    1.5,
+    -1.5
+};
+
+double (*f[N])(double *) = {
+    f0,
+    f1
+};
+
+double (*ff[N][N])(double *) = {
+    { f00, f01 },
+    { f10, f11 }
+};
+
+double fx[N];
+double ffx[N][N];
+double ffx1[N][N];
+
+double nevyazka;
+
+void printX()
+{
+    for (int i = 0; i < N; i++)
+        printf("%10f ", x[i]);
+    printf("\n");
 }
 
-double function2(double x, double y) {
-    return x - y * y + 2;
+void print(int t)
+{
+    printf("%3i | %12f | ", t, nevyazka);
+    printX();
 }
 
-bool stop_iteration() {
-    if ()
+int main(){
+    for (int t = 0; t < 100; t++){
     
-    return true;
-}
+        for (int i = 0; i < N; i++) {
+            fx[i] = f[i](x);
+        }
+        
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                ffx[i][j] = ff[i][j](x);
+            }
+        }
+    
+        memset(ffx1, 0, N*N*sizeof(double));
 
-int main() {
-    short e[n][n], i = 0, j = 0;
-    double f[n];
+        for (int i = 0; i < N; i++)
+            ffx1[i][i] = 1;
+        
+        double  r;
     
-    x.push_back(X);
-    y.push_back(Y);
-    
-    for (; i < n; i++) {
-        for (; j < n; j++) {
-            e[i][j] = (i == j) ? 1 : 0;
+        for (int i = 0; i < N; i++)
+        {
+            if (ffx[i][i] == 0) {
+                for (int p = i+1; p < N; p++) {
+                    if (ffx[p][i] != 0) {
+                        // если нашли нужную строку, то меняем строки i и p местами
+                        for (int q = 0; q < N; q++) {
+                            int t = ffx[i][q];
+                            ffx[i][q] = ffx[p][q];
+                            ffx[p][q] = t;
+                        }
+                        // поменяли строки? продолжаем алгоритм, прервав цикл
+                        break;
+                    }
+                }
+            }
+            // если замены нулевого элемента не было
+            if (ffx[i][i] == 0) {
+                printf("[!] Devizion by zero in 'Gauss method'\n");
+                return 1;
+            }
+            
+            for (int k = 0; k < N; k++) {
+                if (i == k) continue;
+
+                r = ffx[k][i] / ffx[i][i];
+                
+                for (int j = 0; j < N; j++) {
+                    ffx[k][j] -= r * ffx[i][j];
+                    ffx1[k][j] -= r * ffx1[i][j];
+                }
+            }
+        }
+        
+        for (int i = 0; i < N; i++) {
+            r = ffx[i][i];
+            for (int j = 0; j < N; j++) {
+                ffx[i][j] /= r;
+                ffx1[i][j] /= r;
+            }
+            
+        }
+        
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                double dx = 0;
+                for (int k = 0; k < N; k++)
+                    dx += ffx1[i][k] * ff[k][j](x);
+                printf("%10.2lf\t", fabs(dx));
+            }
+            printf("\n");
+        }
+        printf("\n\n");
+        
+        for (int i = 0; i < N; i++) {
+            double dx = 0;
+            for (int k = 0; k < N; k++)
+                dx += ffx1[i][k] * fx[k];
+            x[i] = x[i] - dx;
+        }
+        
+        nevyazka = 0;
+        for (int i = 0; i < N; i++)
+            nevyazka += SQR( f[i](x) );
+        nevyazka = sqrt(nevyazka);
+        
+        print(t);
+        
+        if (nevyazka < EPS) {
+            break;
+        }
+        
+        if (isnan(nevyazka)) {
+            break;
         }
     }
-    
-    do {
-        
-    } while(stop_iteration())
     
     return 0;
 }
